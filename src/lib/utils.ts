a1a33c965eaf6cba9fed0a4ae7ec6a26db50caad
Tami Maiwashe-Mafuxwana <stahfacedquail@gmail.com>
2025-04-14 13:51:00 +0200

    Improve accuracy of ordinal suffix function

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,8 +1,11 @@
 export const getOrdinalSuffix = (i: number) => {
-  switch(i) {
-    case 1: return "st";
-    case 2: return "nd";
-    case 3: return "rd";
-    default: return "th";
-  }
+  const rem = i % 100;
+
+  if (rem === 11) return "th";
+  if (rem === 12) return "th";
+  if (rem === 13) return "th";
+  if (rem % 10 === 1) return "st";
+  if (rem % 10 === 2) return "nd";
+  if (rem % 10 === 3) return "rd";
+  return "th";
 }
